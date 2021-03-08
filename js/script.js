522a1929cdaf8f3e8420d267324df8f1c536a529
bobbyturban <41744431+bobbyturban@users.noreply.github.com>
2021-03-08 10:56:59 +0100

    Draft_Files von Amanda

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var map = L.map('map').setView([52.01799,9.03725], 13);
+
+// Set up tile layer
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/light-v10',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiYWJqYXJkaW0iLCJhIjoiY2tmZmpyM3d3MGZkdzJ1cXZ3a3kza3BybiJ9.2CgI2GbcJysBRHmh7WwdVA'
+}).addTo(map);
